85ec2b305c8533ea7a4c18ff9d13c7a397ceb73a
MarekMatyas <matyasamarek@gmail.com>
2023-02-01 15:59:16 -0800

    new1

--- calculator_2.py
+++ calculator_2.py
@@ -10,22 +10,32 @@ def multiply(a: int, b: int) -> float:
 def divide(a: int, b: int) -> float:
     return a / b
 
-number_one = float(input("Enter the first number:"))
-#This line of code will ask for the user to input the first number
-number_two = float(input("Enter the second number:"))
-# This line asks for the second number
+def convert(a: int,) -> float:
+    return a * 0.01
 
-user_choice = input("What calculation is desired? ('add', 'sub', 'multi', 'div'):")
-#This line of code with ask the user to choose a calculation method
+# def m_convert_ft(a: int) -> float:
+#     return a * 3.28084
 
 
-if user_choice == "add": # This line opens an if statement stating if users choice is "add" then:
-    print(add(number_one, number_two)) #  prints out the output using the function above
-elif user_choice == "sub":
-    print(subtract(number_one, number_two))
-elif user_choice == "multi":
-    print(multiply(number_one, number_two))
-elif user_choice == "div":
-    print(divide(number_one, number_two))
+
+user_choice = input("What calculation is desired? ('add', 'sub', 'multi', 'div', 'conv'):")
+
+if user_choice == "conv":
+    convert_choice = float(input("Please enter the number you want to convert in meters: "))
+    print(convert(convert_choice))
+    print(convert_choice * 3.28084)
 else:
-    print("Wrong calculation, please try again")# If user input wrong calculation method this message will print out.
+    number_one = float(input("Enter the first number:"))
+    number_two = float(input("Enter the second number:"))
+    if user_choice == "add":  # This line opens an if statement stating if users choice is "add" then:
+        print(add(number_one, number_two))  # prints out the output using the function above
+    elif user_choice == "sub":
+        print(subtract(number_one, number_two))
+    elif user_choice == "multi":
+        print(multiply(number_one, number_two))
+    elif user_choice == "div":
+        print(divide(number_one, number_two))
+    else:
+        print("Wrong calculation, please try again")  # If user input wrong calculation method this message will print out.
+
+
